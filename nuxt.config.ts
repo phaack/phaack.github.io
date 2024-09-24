06be3e674058ce8bee78271772dfaad00788ddcf
phaack <phaack.edu@gmail.com>
2024-09-24 14:54:16 +0200

    state management rework

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,22 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt'
   ],
+
+  //app head
+  app: {
+    head: {
+      title: 'Pretty Table Builider',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Pretty Table Builder to download images of tables' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
+  
   pinia: {
     storesDirs: ['./stores/**'],
   },
